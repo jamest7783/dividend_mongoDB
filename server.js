25c28b1fc25b67437024cc6f4ad5848375e11d9a
James Jewitt <jamestjewitt@gmail.com>
2022-09-12 14:35:20 -0400

    Init connection to MongoAtlas

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(logger('dev'))
-app.use(express.static(`${__dirname}/client/build`))
 
 const PORTMONGO=process.env.PORT||3001
 const db=require('./db')
@@ -16,7 +15,6 @@ const routes=require('./routes')
 
 app.get('/mongo',(req,res)=>res.json({message:'Mongo server hit!'}))
 app.use('/api',routes)
-app.get('/*', (req,res) => {res.sendFile(`${__dirname}/client/build/index.html`)})
 app.listen(PORTMONGO,()=>{console.log(`Express server paired with Mongo listening on port: ${PORTMONGO}`)})
 
 
